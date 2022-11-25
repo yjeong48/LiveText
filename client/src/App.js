3d85dbc07aa86ea2adaa20c2d519e42e662d5f51
yjeong48 <71805570+yjeong48@users.noreply.github.com>
2022-11-25 15:41:18 -0600

    final update

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,255 @@
-import axios from 'axios';
-import React,{Component} from 'react';
+import axios from "axios";
+import React, { Component } from "react";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import "./style.css";
 
 class App extends Component {
-	state = {
-	// Initially, no file is selected
-	selectedFile: null,
-	target_lang: ""
-	};
+  allowedFileTypes = "image/png, image/jpg, image/jpeg";
 
-	// After selecting file from pop up
-	onFileChange = event => {
-	this.setState({ selectedFile: event.target.files[0] });	
-	};
-	
-	// After clicking the translate button)
-	onFileUpload = () => {
-	
-	const formData = new FormData();
-	formData.append(
-		"file",
-		this.state.selectedFile,
-		this.state.selectedFile.name
-	);
-	
-	// Details of the uploaded file
-	console.log(this.state.selectedFile);
-	
-	// Request made to the backend api
-	axios.post("http://localhost:5000", formData)
-	.then(response => this.setState({ translated_text: response.data}))
-	.catch(function (error) {
-		console.log(error)
-	})
-	}
-	
-	//Display translated text
-	translated = () => {
-		return (
-			<div>
-				<p>Translated text: </p>
-				<p>{this.state.translated_text}</p>
-			</div>
-		)
-	}
-	
-	render() {
-	return (
-		<div style={{textAlign:"center"}}>
-			<h1>
-			OCR Personal Project
-			</h1>
-			<h3>
-			Translate text from any image to Korean
-			</h3>
-			<div>
-				<input type="file" onChange={this.onFileChange} />
-				<button onClick={this.onFileUpload}>
-				Translate!
-				</button>
-			</div>
-		{this.translated()}
-		</div>
-	);
-	}
+  state = {
+    initialFile: null,
+    selectedFile: null,
+    targetLang: "",
+    translatedText: "",
+    isLoading: false,
+    preview: null,
+    crop: {
+      unit: "%",
+      width: 0,
+      height: 0,
+    },
+  };
+
+  onLanguageChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ targetLang: event.target.value });
+  };
+
+  onFileChange = (event) => {
+    this.setState({
+      crop: {
+        unit: "%",
+        width: 0,
+        height: 0,
+      },
+    });
+    this.setState({ translatedText: "" });
+
+    if (event.target.files && event.target.files.length > 0) {
+      const initialImage = event.target.files[0];
+      this.setState({ selectedFile: initialImage });
+      this.setState({ initialFile: initialImage });
+      const imageReader = new FileReader();
+      imageReader.readAsDataURL(initialImage);
+      imageReader.addEventListener("load", () =>
+        this.setState({ preview: imageReader.result })
+      );
+    }
+  };
+
+  onImageLoaded = (image) => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = (crop) => {
+    this.userCrop(crop);
+  };
+
+  onCropChange = (crop) => {
+    this.setState({ crop });
+  };
+
+  async userCrop(crop) {
+    if (this.imageRef && crop.width && crop.height) {
+      await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        this.state.selectedFile.name
+      );
+    }
+  }
+
+  getCroppedImg(image, crop, fileName) {
+    const canvas = document.createElement("canvas");
+    const pixelRatio = window.devicePixelRatio;
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = crop.width * pixelRatio * scaleX;
+    canvas.height = crop.height * pixelRatio * scaleY;
+
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = "high";
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width * scaleX,
+      crop.height * scaleY
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (!blob) {
+          reject(new Error("Canvas is empty"));
+          console.error("Canvas is empty");
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+        const newImage = new File([blob], blob.name, { type: blob.type });
+        this.setState({ selectedFile: newImage });
+        console.log(this.state.selectedFile);
+      });
+    });
+  }
+
+  onFileUpload = () => {
+    this.setState({ translatedText: "" });
+
+    if (this.state.selectedFile) {
+      this.setState({ isLoading: true });
+      const formData = new FormData();
+
+      formData.append(
+        "file",
+        this.state.selectedFile,
+        this.state.selectedFile.name
+      );
+
+      formData.append("targetLang", this.state.targetLang);
+      // check details of the uploaded file
+      console.log(this.state.selectedFile);
+
+      axios
+        .post("https://livetext-flask.herokuapp.com/translate", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          this.setState({ isLoading: false });
+          this.setState({ translatedText: response.data });
+        })
+        .catch((error) => {
+          // Error
+          if (error.response) {
+            // server responded with status code
+            this.setState({ isLoading: false });
+            this.setState({ translatedText: "Something went wrong :(" });
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // request was made but no response was received
+            this.setState({ isLoading: false });
+            this.setState({ translatedText: "Try including more context" });
+            console.log(error.request);
+          } else {
+            // error in request made
+            this.setState({ isLoading: false });
+            this.setState({ translatedText: "Something went wrong :(" });
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    }
+  };
+
+  onReset = () => {
+    this.setState({
+      crop: {
+        unit: "%",
+        width: 0,
+        height: 0,
+      },
+    });
+    this.setState({ selectedFile: this.state.initialFile });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>OCR Image Text Translator</h1>
+        <h2>A cloud-based smart tool for polyglots</h2>
+        <div>
+          <label>Translate to: </label>
+          <select
+            className="myButton"
+            value={this.state.targetLang}
+            onChange={this.onLanguageChange}
+          >
+            <option value="en">English</option>
+            <option value="zh-Hans">Chinese</option>
+            <option value="da">Danish</option>
+            <option value="fr">French</option>
+            <option value="de">German</option>
+            <option value="el">Greek</option>
+            <option value="it">Italian</option>
+            <option value="ja">Japanese</option>
+            <option value="ko">Korean</option>
+            <option value="pt">Portuguese</option>
+            <option value="es">Spanish</option>
+          </select>
+        </div>
+        <div>
+          <input
+            type="file"
+            accept={this.allowedFileTypes}
+            onChange={this.onFileChange}
+            disabled={this.state.isLoading}
+          />
+          <button
+            className="myButton"
+            onClick={this.onFileUpload}
+            disabled={this.state.isLoading}
+          >
+            Translate
+          </button>
+        </div>
+        {this.state.selectedFile && (
+          <p id="guide">You can select a specific area to translate!</p>
+        )}
+        <div>
+          <ReactCrop
+            src={this.state.preview}
+            crop={this.state.crop}
+            onImageLoaded={this.onImageLoaded}
+            onComplete={this.onCropComplete}
+            onChange={this.onCropChange}
+            crossorigin={null}
+          />
+        </div>
+        {this.state.selectedFile && (
+          <button className="myButton" onClick={this.onReset}>
+            Reset
+          </button>
+        )}
+        <div>
+          <PropagateLoader
+            className="text-container"
+            loading={this.state.isLoading}
+            color="#36d7b7"
+          />
+          <div className="text-container">
+            {this.state.translatedText && (
+              <p id="translated">{this.state.translatedText}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,53 +1,44 @@
-from flask import Flask, flash, request, redirect, url_for 
+from flask import Flask, flash, request, redirect, url_for
 import flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS
-from werkzeug.utils import secure_filename 
+from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 import os
+import io
 import time
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes, VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
 import sys
-import requests, uuid, json
+import requests
+import uuid
+import json
 
-UPLOAD_FOLDER = '/Users/yoonsungjeong/personalproject/pythonProject/flask-server/images' 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']) 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
-cors = CORS(app)
+CORS(app)
 load_dotenv()
 subscription_key = os.getenv('COG_SERVICE_KEY')
 location = os.getenv('COG_SERVICE_REGION')
 
-def get_text(image_path, computervision_client):
-    #read image from saved folder
-    read_image = open(image_path, "rb")
 
-    # Call API with image and raw response (allows you to get the operation location)
-    read_response = computervision_client.read_in_stream(read_image, raw=True)
-    # Get the operation location (URL with ID as last appendage)
+def get_text(image, computervision_client):
+    read_response = computervision_client.read_in_stream(image, raw=True)
     read_operation_location = read_response.headers["Operation-Location"]
-    # Take the ID off and use to get results
     operation_id = read_operation_location.split("/")[-1]
 
-    read_image.close()
+    image.close()
 
-    #t1 = time.time()
-    # Call the "GET" API and wait for it to retrieve the results 
-    i=0
+    i = 0
     while True:
-        i+=1
+        i += 1
         read_result = computervision_client.get_read_result(operation_id)
         if read_result.status not in ['notStarted', 'running']:
             break
         time.sleep(1)
-    #t2 = time.time()
-    #print("time taken: ", t2-t1)
 
-    # Print the detected text, line by line
     if read_result.status == OperationStatusCodes.succeeded:
         text = ""
         for text_result in read_result.analyze_result.read_results:
@@ -55,29 +46,32 @@ def get_text(image_path, computervision_client):
                 text += line.text
     return text
 
+
 def detect_language(text, subscription_key, location, constructed_url):
     params = {
-            "api-version": "3.0"
-        }
+        "api-version": "3.0"
+    }
     headers = {
-    "Ocp-Apim-Subscription-Key": subscription_key,
-    "Ocp-Apim-Subscription-Region": location,
-    "Content-type": "application/json"
+        "Ocp-Apim-Subscription-Key": subscription_key,
+        "Ocp-Apim-Subscription-Region": location,
+        "Content-type": "application/json"
     }
     body = [{
         "text": text
     }]
-    # Send the request and get response
-    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    request = requests.post(
+        constructed_url, params=params, headers=headers, json=body)
     response = request.json()
     language = response[0]["language"]
+    print("source language is: ", response[0])
     return language
 
-def translate(text, source_language, subscription_key, location, constructed_url):
+
+def translate(text, source_language, target_language, subscription_key, location, constructed_url):
     params = {
         'api-version': '3.0',
         'from': source_language,
-        'to': ['ko']
+        'to': target_language
     }
 
     headers = {
@@ -87,53 +81,56 @@ def translate(text, source_language, subscription_key, location, constructed_url
         'X-ClientTraceId': str(uuid.uuid4())
     }
 
-
     body = [{
         'text': text
     }]
 
-    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    request = requests.post(
+        constructed_url, params=params, headers=headers, json=body)
     response = request.json()
-
     translation = response[0]["translations"][0]["text"]
     return translation
 
-def allowed_file(filename): 
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/", methods=["POST", "GET"])
 def my_translator():
     if request.method == 'GET':
-        return flask.Response("flask server is running", headers={"Content-Type":"text/html"})
+        return flask.Response("flask server is running", headers={"Content-Type": "text/html"})
     image_path = ""
-    # check if the post request has the file part 
-    if 'file' not in request.files: 
+    # check if the post request has the file part
+    if 'file' not in request.files:
         print("file not in request")
-        return flask.Response("Request does not contain image file.", headers={"Content-Type":"text/html"})
-    
-    file = request.files['file'] 
-    if file and allowed_file(file.filename): 
-        filename = secure_filename(file.filename) 
-        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(image_path) 
+        return flask.Response("Request does not contain image file.", headers={"Content-Type": "text/html"})
 
-        #Authenticate Computer Vision client
+    file = request.files['file']
+    target_lang = request.form.get("targetLang")
+    print(request, "\n")
+    if target_lang == "":
+        target_lang = "en"
+    if file and allowed_file(file.filename):
+        # read image file as bytes into file_like object
+        image = io.BytesIO(file.read())
+
+        # Authenticate Computer Vision client
         endpoint = "https://livetext.cognitiveservices.azure.com/"
         trans_endpoint = "https://api.cognitive.microsofttranslator.com"
-        computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+        computervision_client = ComputerVisionClient(
+            endpoint, CognitiveServicesCredentials(subscription_key))
         detect_constructed_url = trans_endpoint + '/detect'
         trans_constructed_url = trans_endpoint + '/translate'
-        
-        text = get_text(image_path, computervision_client)
-        source_language = detect_language(text, subscription_key, location, detect_constructed_url)
-        translated_text = translate(text, source_language, subscription_key, location, trans_constructed_url)
 
-        return flask.Response(translated_text, headers={"Content-Type":"text/html"})
+        text = get_text(image, computervision_client)
+        source_language = detect_language(
+            text, subscription_key, location, detect_constructed_url)
+        translated_text = translate(
+            text, source_language, target_lang, subscription_key, location, trans_constructed_url)
+        image.close()
+        return flask.Response(translated_text, headers={"Content-Type": "text/html"})
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True, threaded=True)
-
- 
- 
+    app.run(debug = False, threaded=True)
